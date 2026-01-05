be0311b250541469d3f69b527cbc2b4169ee93ed
VINYTHASV1708 <vinytha.sv2023@vitstudent.ac.in>
2026-01-05 16:02:54 +0530

    Add files via upload

---
+++ ShoppingBill.java
@@ -0,0 +1,16 @@
+package Shopping;
+
+public class ShoppingBill {
+	public static void main(String[] args) {
+		
+		double price = 20.00;		
+		int qty = 5;
+		
+		double total = price * qty;
+		double discount = total * 0.10;
+		double finalAmount = total - discount;
+		System.out.println("Total Amount:" + total);
+		System.out.println("Discount: " + discount);
+		System.out.println("Final Payable Amount:" + finalAmount);
+	}
+}
